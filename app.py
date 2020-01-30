3347466a9aab1edb8bcdb3bfa3a77b3d63a8a35d
ecundick <ecundick@outlook.com>
2020-01-30 21:23:38 +0000

    Connected to DB and imported required libraries.

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "jargon-dictionary"
+app.config["MONGO_URI"] = os.getenv("JARGON_URI")
+
+mongo = PyMongo(app)
+
+
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to below enables debug output. This line should be removed before deploying a production app.
+    app.debug = True
+    app.run()
