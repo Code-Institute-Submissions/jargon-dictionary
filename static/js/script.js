e056e95424c369b517d79435d71950897e060ea8
ecundick <ecundick@outlook.com>
2020-02-13 18:44:10 +0000

    Hidden submit buttons unless forms has values

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+$('.hidden').hide() //Hides submit button on add pages
+
 // Checks to see if description is empty yes return false else true
 function checkDesc() {
     if ($('#description').val() === "") {
@@ -24,12 +26,36 @@ function checkWord() {
     }
 }
 
+// Checks too see if form can be submitted if so shows submit button
+function addDeckFormSubmit(){
+    if(checkName() && checkDesc()){
+        $('.hidden').show()
+        return true;
+       
+    }else{
+        $('.hidden').hide()
+        return false
+    }
+}
+
+function addDefintionFormSubmit(){
+    if(checkDefinition() && checkWord()){
+        $('.hidden').show()
+        return true;
+       
+    }else{
+        $('.hidden').hide()
+        return false
+    }
+}
+
 // Checks to see if definition is empty yes return false else true
 function checkDefinition() {
     if ($('#definition').val() === "") {
         return false;
     } else {
         return true;
+        
     }
 }
 $(document).ready(function () {
@@ -41,9 +67,11 @@ $(document).ready(function () {
         if (checkName() == true) {
             $('#name').css("border", "2px solid green");
             $('.deck-name-check').html('')
+            addDeckFormSubmit()
         } else {
             $('#name').css("border", "2px solid red")
             $('.deck-name-check').html('Please enter a deck name')
+            addDeckFormSubmit()
         }
     });
 
@@ -52,9 +80,11 @@ $(document).ready(function () {
         if (checkDesc()) {
             $('#description').css("border", "2px solid green");
             $('.deck-description-check').html('')
+            addDeckFormSubmit()
         } else {
             $('#description').css("border", "2px solid red");
             $('.deck-description-check').html('Please enter a description');
+            addDeckFormSubmit()
         }
     })
 
@@ -64,9 +94,11 @@ $(document).ready(function () {
         if (checkWord()) {
             $('#word').css("border", "2px solid green");
             $('.word-check').html('')
+            addDefintionFormSubmit()
         } else {
             $('#word').css("border", "2px solid red");
             $('.word-check').html('Please enter a word');
+            addDefintionFormSubmit()
         }
     })
 
@@ -75,9 +107,11 @@ $(document).ready(function () {
         if (checkDefinition()) {
             $('#definition').css("border", "2px solid green");
             $('.definition-check').html('')
+            addDefintionFormSubmit()
         } else {
             $('#definition').css("border", "2px solid red");
             $('.definition-check').html('Please enter a word');
+            addDefintionFormSubmit()
         }
     })
 
