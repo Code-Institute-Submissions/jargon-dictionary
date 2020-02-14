8917de9012b267f7d22069ae0e8c7b9d93ab7dca
ecundick <ecundick@outlook.com>
2020-02-14 13:17:43 +0000

    Added deployment to readme + application to app.py

--- README.md
+++ README.md
@@ -10,4 +10,21 @@
 - Users must be able to see definitions that belong to a deck after clicking on deck. 
 
 
-## Deployment
+## Deployment 
+
+
+### Database 
+
+
+
+### Web Application 
+
+This application was deployed using Elastic Beanstalk on AWS. To do this I used the Elastic Beanstalk CLI.  
+
+I created an .ebignore file, this was is used by Elastic Beanstalk and excludes irrelevant folders from the application upload, e.g. any virtual environments, tests etc. 
+
+I then ran the command  pip freeze > requirements.txt this is used by elastic beanstalk to see what packages are required to be installed i.e. Flask, Jinja et.  
+
+I then ran the command eb init -p python-3.6 <application name> --region eu-west-2 this creates the application within AWS. 
+
+Next I ran the command eb create prod this created the environment and deployed my code into it.  
--- app.py
+++ app.py
@@ -3,40 +3,40 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "jargon-dictionary"
-app.config["MONGO_URI"] = os.getenv("JARGON_URI")
+application = Flask(__name__)
+application.config["MONGO_DBNAME"] = "jargon-dictionary"
+application.config["MONGO_URI"] = os.getenv("JARGON_URI")
 
-mongo = PyMongo(app)
+mongo = PyMongo(application)
 
 
 # Index
-@app.route('/')
+@application.route('/')
 def get_decks():
     return render_template('index.html', categories=mongo.db.category.find())
 
 # DEFINITION STARTS HERE
 
 # Dynamic URL for getting definitions based on category
-@app.route('/get_definitions/<category>')
+@application.route('/get_definitions/<category>')
 def get_definitions(category):
     return render_template("definitions.html", category=category, definitions=mongo.db.jargon.find({"category": category}))
 
 #URL to add definitions
-@app.route('/add_definition')
+@application.route('/add_definition')
 def add_definition():
     categories = mongo.db.category.find()
     return render_template('add-definition.html',  categories=categories)
 
 #Inserts definitions to database, takes POST method. 
-@app.route('/insert_definition', methods=['POST'])
+@application.route('/insert_definition', methods=['POST'])
 def insert_definition():
     definitions = mongo.db.jargon # Gets definitions database object
     definitions.insert(request.form.to_dict()) # Inserts form values into db. 
     return redirect(url_for('get_decks'))
 
 # Dynamic URL for edit definition, depending on definition clicked it will take the id and pass it to the function
-@app.route('/edit_definition/<definition_id>')
+@application.route('/edit_definition/<definition_id>')
 def edit_definition(definition_id):
     definition = mongo.db.jargon.find_one({"_id": ObjectId(definition_id)}) # Finds definition in DB
     categories = mongo.db.category.find() # Find all categories in DB
@@ -44,7 +44,7 @@ def edit_definition(definition_id):
 
 
 # Updates definition in DB
-@app.route('/update_definition/<definition_id>', methods=["POST"])
+@application.route('/update_definition/<definition_id>', methods=["POST"])
 def update_definition(definition_id):
     definitions = mongo.db.jargon
     current_definition = mongo.db.jargon.find_one({"_id": ObjectId(definition_id)})
@@ -60,7 +60,7 @@ def update_definition(definition_id):
     return redirect(url_for('get_definitions', category=category))
 
 # Deletes definition in DB
-@app.route('/delete_definition/<definition_id>')
+@application.route('/delete_definition/<definition_id>')
 def delete_definition(definition_id):
     mongo.db.jargon.remove({"_id": ObjectId(definition_id)})
     return redirect(url_for('get_decks'))
@@ -70,27 +70,27 @@ def delete_definition(definition_id):
 # DECK STARTS HERE
 
 # URL for adding a deck 
-@app.route('/add_deck')
+@application.route('/add_deck')
 def add_deck():
     return render_template('add-deck.html')
 
 
 # Inserts deck to db, takes POST method
-@app.route('/insert_deck', methods=['POST'])
+@application.route('/insert_deck', methods=['POST'])
 def insert_deck():
     decks = mongo.db.category
     decks.insert(request.form.to_dict())
     return redirect(url_for('get_decks'))
 
 # Dynamic URL for editing Deck, takes deck_id and finds the deck in DB
-@app.route('/edit_deck/<deck_id>')
+@application.route('/edit_deck/<deck_id>')
 def edit_deck(deck_id):
     deck_id = mongo.db.category.find_one({"_id": ObjectId(deck_id)}) 
     return render_template('edit-deck.html', deck=deck_id)
 
 
 # Updates deck in DB takes deck_id to use for update and accepts POST
-@app.route('/update_deck/<deck>', methods=["POST"])
+@application.route('/update_deck/<deck>', methods=["POST"])
 def update_deck(deck):
     decks = mongo.db.category
     deck = mongo.db.category.find_one({"_id": ObjectId(deck)})
@@ -104,15 +104,15 @@ def update_deck(deck):
     return redirect(url_for('get_decks'))
 
 # Deletes deck by taking deck_id, also deletes any definitions with category that matches deck name
-@app.route('/delete_deck/<deck_id>')
+@application.route('/delete_deck/<deck_id>')
 def delete_deck(deck_id):
     deck = mongo.db.category.find_one({"_id": ObjectId(deck_id)}) # used to find definitions
     mongo.db.jargon.remove({"category": deck.get('name')})
     mongo.db.category.remove({"_id": ObjectId(deck_id)})
     return redirect(url_for('get_decks'))
 
-# run the app.
+# run the application.
 if __name__ == "__main__":
-    # Setting debug to below enables debug output. This line should be removed before deploying a production app.
-    app.debug = True
-    app.run()
+    # Setting debug to below enables debug output. This line should be removed before deploying a production application.
+    application.debug = True
+    application.run()
